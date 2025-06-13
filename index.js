e93d33a7ee8ec5f038fa483fb5c812f8a7d1fe7c
dspocius <dspocius@gmail.com>
2025-06-13 11:46:53 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,23 @@
-export default {
+// index.js
+var index_default = {
   async fetch(request, env, ctx) {
     if (new URL(request.url).pathname === "/") {
-      const apiRes = await fetch("https://api-manager.upbit.com/api/v1/announcements?os=web&page=1&per_page=1&category=trade&nocache=true&timestamp="+new Date().getTime());
+      const apiRes = await fetch("https://api-manager.upbit.com/api/v1/announcements?os=web&page=1&per_page=1&category=trade&nocache=true&timestamp=" + (new Date()).getTime());
       const data = await apiRes.json();
-
       return new Response(JSON.stringify(data), {
-        headers: { "Content-Type": "application/json" },
+        headers: { "Content-Type": "application/json" }
+      });
+    } else if (new URL(request.url).pathname === "/ipas") {
+      const apiRes = await fetch("https://api.ipify.org?format=json");
+      const data = await apiRes.json();
+      return new Response(JSON.stringify(data), {
+        headers: { "Content-Type": "application/json" }
       });
     }
-
+ 
     return new Response("Not Found", { status: 404 });
-  },
+  }
 };
+export {
+  index_default as default
+}; 
